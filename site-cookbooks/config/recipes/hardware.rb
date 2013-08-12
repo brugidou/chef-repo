d807a1a2f0876bf842d1cd363d0b11ef447ff53f
Gregoire Seux <g.seux@criteo.com>
2013-08-12 14:07:43 +0000

    Restore backlight control after suspend

--- site-cookbooks/config/recipes/hardware.rb
+++ site-cookbooks/config/recipes/hardware.rb
@@ -28,3 +28,10 @@ end
 execute "amixer sset Master unmute" do
   only_if "amixer sget Master | grep 'off]'"
 end
+
+#Backlight control after sleep
+cookbook_file "/etc/pm/sleep.d/99backlight" do
+  owner "root"
+  group "root"
+  mode "0744"
+end
